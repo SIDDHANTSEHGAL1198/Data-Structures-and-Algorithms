76c105b106a0ccb421ad3801f265c4cf4275457d
Siddhant Sehgal <siddhusehgal264@gmail.com>
2021-07-16 02:28:40 +0530

    bst_search

---
+++ Binary Search Tree/bst_search.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+using namespace std;
+struct node 
+{
+    int data;
+    struct node *left;
+    struct node *right;
+    node(int val)
+    {
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+struct node *root=NULL;
+node *create_binary_search_tree(node *root,int val)
+{
+    if(root==NULL)
+    {
+        return new node(val);
+    }
+    if(val<root->data)
+    {
+        root->left=create_binary_search_tree(root->left,val);
+    }
+    else
+    {
+        root->right=create_binary_search_tree(root->right,val);
+    }
+
+    return root;
+}
+
+void PreOrder_Traversal(node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    cout<<root->data<<" ";
+    PreOrder_Traversal(root->left);
+    PreOrder_Traversal(root->right);
+}
+
+void InOrder_Traversal(node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    InOrder_Traversal(root->left);
+    cout<<root->data<<" ";
+    InOrder_Traversal(root->right);
+}
+
+void PostOrder_Traversal(node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    PostOrder_Traversal(root->left);
+    PostOrder_Traversal(root->right);
+    cout<<root->data<<" ";
+}
+
+node *bst_search(node *root,int ele)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    if(root->data==ele)
+    {
+        return root;
+    }
+    if(root->data>ele)
+    {
+        bst_search(root->left,ele);
+    }
+    if(root->data<ele)
+    {
+        bst_search(root->right,ele);
+    }
+
+}
+
+int main()
+{
+    cout<<"Enter 1 to create Binary Search Tree"<<endl;
+    cout<<"Enter 2 for PreOrder Traversal"<<endl;
+    cout<<"Enter 3 for InOrder Traversal"<<endl;
+    cout<<"Enter 4 for PostOrder Traversal"<<endl;
+    cout<<"Enter 5 for Searching an element"<<endl;
+
+    int op=0;
+    do
+    {
+        cout<<endl<<"Enter opeartion of your choice"<<endl;
+        cin>>op;
+
+        switch(op)
+        {
+            case 1:
+            {
+                int size;
+                cout<<"Enter size of array for BST"<<endl;
+                cin>>size;
+
+                int arr[100000];
+                cout<<"Enter array elements"<<endl;
+                for(int i=0;i<size;i++)
+                {
+                    cin>>arr[i];
+                }
+
+                for(int i=0;i<size;i++)
+                {
+                    if(i==0)
+                    {
+                        root=create_binary_search_tree(root,arr[i]);
+                    }
+                    else
+                    {
+                        create_binary_search_tree(root,arr[i]); 
+                    }
+                }
+                break;
+
+            }
+
+            case 2:
+            {
+                cout<<"Preorder Traversal"<<endl;
+                PreOrder_Traversal(root);
+                break;
+            }
+            
+            case 3:
+            {
+                cout<<"Inorder_Traversal"<<endl;
+                InOrder_Traversal(root);
+                break;
+            }
+
+            case 4:
+            {
+                cout<<"Postorder Traversal"<<endl;
+                PostOrder_Traversal(root);
+                break;
+            }
+
+            case 5:
+            {
+                int ele=0;
+
+                cout<<"Enter element you want to search"<<endl;
+                cin>>ele;
+
+                node *res=bst_search(root,ele);
+                if(res==NULL)
+                {
+                    cout<<"Element not found"<<endl;
+                }
+                else
+                {
+                    cout<<"Element found"<<endl;
+                }
+
+                break;
+            }
+        }
+    } while (op!=99);
+    cout<<"Done and dusted"<<endl;
+    
+    return 0;
+}
